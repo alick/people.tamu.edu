b70d435ac005edfb19ce72238f15f582c5d136db
Alick Zhao <alick@localhost.localdomain>
2016-01-16 14:53:29 -0600

    bibtex.js: add tip on IEEE_abrv

--- bibtex.js
+++ bibtex.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('.bibbtn').on('click', function(){
         $(this).next().toggle();
     });
+    $('.bib').filter(function(){
+        if (true === /journal\s+=\s+IEEE_/.test($(this).text())) {
+            $(this).attr('title', 'Use with IEEEabrv.bib');
+        }
+    });
 });
